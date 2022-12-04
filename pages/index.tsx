f31b8c63a76c49bc53fbe0faa4b15f07b7b17167
Dellyson <isiakalukmandellyson@gmail.com>
2022-12-04 14:39:07 +0100

    initial commit 🚀🚀🚀

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,38 +1,51 @@
-import { Box, Group, Paper, Stack, Text } from '@mantine/core';
+import { Box, Group, Paper, Skeleton, Stack, Text } from '@mantine/core';
 import { getAllPostsData } from '../src/api/posts';
 import { MainLayout } from '../src/components/layout/main';
 
 export default function HomePage() {
-  const { posts } = getAllPostsData();
+  const { posts, isLoading } = getAllPostsData();
+  const postsList = posts?.map((post: any) => (
+    <div>
+      <Group noWrap position="apart">
+        <Text size="sm">
+          <a href={`${post.url}`}>{post.title}</a>
+        </Text>
+        <Text size="xs">({post.url.split('.')[1]})</Text>
+      </Group>
+      {post.description.length > 1 && (
+        <Text size={10} mt={5}>
+          {post.description}
+        </Text>
+      )}
+      <Text size={10} mt={5} color="brand.4">
+        {post.date}
+      </Text>
+    </div>
+  ));
+
+  const postsListLoading = [1, 2, 3, 4, 5, 6, 7, 9, 10].map((it) => (
+    <Stack spacing={5} key={it}>
+      <Group noWrap position="apart">
+        <Skeleton height={10} width="100" />
+        <Skeleton height={10} width="100" />
+      </Group>
+      {[1, 2].map((item) => (
+        <Skeleton height={10} key={item} width="100" />
+      ))}
+      <Text size={10} mt={5} color="brand.4">
+        <Skeleton height={10} width="100" />
+      </Text>
+    </Stack>
+  ));
 
   return (
     <MainLayout>
       <Box>
-        {' '}
         <Text size="sm" weight={700} mt={5} color="brand.4">
           Welcome to the Crypto Insights, the largest collection of crypto news and insights.
         </Text>
         <Paper p="md" shadow="sm" mt={20} color="white">
-          <Stack spacing="md">
-            {posts.map((post: any) => (
-              <div>
-                <Group noWrap position="apart">
-                  <Text size="sm">
-                    <a href={`${post.url}`}>{post.title}</a>
-                  </Text>
-                  <Text size="xs">({post.url.split('.')[1]})</Text>
-                </Group>
-                {post.description.length > 1 && (
-                  <Text size={10} mt={5}>
-                    {post.description}
-                  </Text>
-                )}
-                <Text size={10} mt={5} color="brand.4">
-                  {post.date}
-                </Text>
-              </div>
-            ))}
-          </Stack>
+          <Stack spacing="md">{isLoading ? postsListLoading : postsList}</Stack>
         </Paper>
       </Box>
     </MainLayout>
